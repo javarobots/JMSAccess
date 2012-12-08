3eb174e8bf8e430920f642df6e05409064b3c484
javarobots74 <javarobots74@gmail.com>
2012-12-07 20:27:57 -0600

    Added throws to methods

--- src/database/MSAccessConnection.java
+++ src/database/MSAccessConnection.java
@@ -11,42 +11,45 @@ import java.util.logging.Logger;
  * @author javarobots74
  */
 public class MSAccessConnection {
-    
+
     private MSAccessConfiguration configuration;
     private Connection connection;
-    
+
     public MSAccessConnection(MSAccessConfiguration configuration){
         this.configuration = configuration;
     }
-    
-    public Connection getConnection(){
-        
+
+    public Connection getConnection()throws SQLException{
         if (connection == null){
-            try {
                 String connectString = "jdbc:odbc:" + configuration.getDataSourceName();
-                connection = DriverManager.getConnection(connectString);                
-            } catch (SQLException ex) {
-                Logger.getLogger(MSAccessConnection.class.getName()).log(Level.SEVERE, null, ex);
-            }
+                connection = DriverManager.getConnection(connectString);
         }
         return connection;
-            
+
     }
-   
+
 //    public static void main(String[] args){
-//        
+//
 //        try {
-//            
+//
 //            Statement stmt = connection.createStatement();
 //            ResultSet rs = stmt.executeQuery("select * from TestTable");
 //            while(rs.next()){
-//                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));                
+//                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
 //            }
 //        }
 //        catch (Exception ex){
-//            
+//
 //        }
-//        
+//
 //    }
-    
+
+    public String getDataSourceName() {
+        return configuration.getDataSourceName();
+    }
+
+    public String getDataSourceFileName() {
+        return configuration.getDataSourceFileName();
+    }
+
 }
--- test/database/MSAccessConnectionTest.java
+++ test/database/MSAccessConnectionTest.java
@@ -1,6 +1,9 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Test;
 
 /**
@@ -8,7 +11,7 @@ import org.junit.Test;
  * @author javarobots74
  */
 public class MSAccessConnectionTest {
-    
+
     public MSAccessConnectionTest() {
     }
 
@@ -17,19 +20,27 @@ public class MSAccessConnectionTest {
      */
     @Test
     public void testGetConnection() {
-        System.out.println("getConnection");
-        MSAccessConfiguration testConfig = new MSAccessConfiguration("test", "test.mdb");
-        MSAccessConnection instance = new MSAccessConnection(testConfig);
-        Connection result = instance.getConnection();
-        org.junit.Assert.assertNotNull(result);
+        try {
+            System.out.println("getConnection");
+            MSAccessConfiguration testConfig = new MSAccessConfiguration("test", "test.mdb");
+            MSAccessConnection instance = new MSAccessConnection(testConfig);
+            Connection result = instance.getConnection();
+            org.junit.Assert.assertNotNull(result);
+        } catch (SQLException ex) {
+            Logger.getLogger(MSAccessConnectionTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     @Test
     public void testInvalidConnection() {
-        System.out.println("invalidConnection");
-        MSAccessConfiguration testConfig = new MSAccessConfiguration("JUnit", "test.mdb");
-        MSAccessConnection instance = new MSAccessConnection(testConfig);
-        Connection result = instance.getConnection();
-        org.junit.Assert.assertNull(result);
+        try {
+            System.out.println("invalidConnection");
+            MSAccessConfiguration testConfig = new MSAccessConfiguration("JUnit", "test.mdb");
+            MSAccessConnection instance = new MSAccessConnection(testConfig);
+            Connection result = instance.getConnection();
+            org.junit.Assert.assertNull(result);
+        } catch (SQLException ex) {
+            Logger.getLogger(MSAccessConnectionTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
